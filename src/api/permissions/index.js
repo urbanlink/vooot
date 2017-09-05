4350ecfb9c648970d32a75cb864b442cfa9e51d0
A. van der Pluijm <info@urbanlink.nl>
2017-09-05 23:19:15 +0200

    basic permissions
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- src/api/permissions/index.js
+++ src/api/permissions/index.js
@@ -3,7 +3,10 @@
 var fs = require('fs');
 var path = require('path');
 var logger = require('../../config/logger');
-
+var models = require('../models/index');
+var settings = require('../../config/settings');
+var auth = require('../../config/auth');
+var utils = require('./../utils');
 
 // General permissions
 var permissions = {};
@@ -17,6 +20,53 @@ fs.readdirSync(__dirname).filter(function(file) {
   var permission = require(path.join(__dirname, file));
   permissions[ name] = permission;
 });
+
+
+permissions.canView = function(req,res,next) {
+  next();
+};
+
+
+/**
+ *
+ * Check if current user can create a new Person
+ *
+ **/
+permissions.canCreate = function(req,res,next) {
+  console.log('Validating canCreate: ', req.user);
+  if (auth.isAdmin(req.user) || auth.isEditor(req.user) ){
+    return next();
+  }
+
+  return utils.handleError(res, 'No access');
+};
+
+
+/**
+ *
+ * Check if current user can update a Person
+ *
+ */
+permissions.canUpdate = function(req,res,next) {
+  if (auth.isAdmin(req.user) || auth.isEditor(req.user) ){
+    return next();
+  }
+  return utils.handleError(res, 'No access');
+};
+
+
+/**
+ *
+ * Check if current user can delete a Person
+ *
+ */
+permissions.canDelete = function(req,res,next) {
+  if (auth.isAdmin(req.user) || auth.isEditor(req.user) ){
+    return next();
+  }
+
+  return utils.handleError(res, 'No access');
+};
 
 
 // Make the peemissions available in the app.
